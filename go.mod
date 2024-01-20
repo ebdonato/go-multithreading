5f96af0a380ca1d5f2709ad0382d648e74d274ab
Eduardo Donato <eduardo.donato@gmail.com>
2024-01-20 13:24:05 -0300

    solve challenge

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ebdonato/go-multithreading
+
+go 1.21.5
+
+require github.com/go-resty/resty/v2 v2.11.0
+
+require golang.org/x/net v0.20.0 // indirect
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type Response struct {
+	Origin string
+	Body   interface{}
+}
+
+const VIA_CEP_URL = "http://viacep.com.br/ws/%s/json/"
+const BRASIL_CEP_URL = "https://brasilapi.com.br/api/cep/v1/%s"
+
+func main() {
+	resultChannel1 := make(chan *Response)
+	resultChannel2 := make(chan *Response)
+
+	cep := os.Args[1]
+	fmt.Printf("CEP: %s\n", cep)
+
+	go func() {
+		resultChannel1 <- makeRequest(fmt.Sprintf(VIA_CEP_URL, cep))
+	}()
+
+	go func() {
+		resultChannel2 <- makeRequest(fmt.Sprintf(BRASIL_CEP_URL, cep))
+	}()
+
+	select {
+	case result1 := <-resultChannel1:
+		printResponse(result1)
+	case result2 := <-resultChannel2:
+		printResponse(result2)
+	case <-time.After(1 * time.Second):
+		fmt.Println("Timeout: A resposta demorou muito para chegar.")
+	}
+
+	fmt.Println("Pronto!")
+}
+
+func printResponse(response *Response) {
+	if len(response.Origin) == 0 {
+		fmt.Println("Nada a exibir.")
+	} else {
+		fmt.Printf("API: %s\n", response.Origin)
+		fmt.Printf("Resposta: %v\n", response.Body)
+	}
+}
+
+func extractDomain(inputURL string) (string, error) {
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", err
+	}
+
+	domain := parsedURL.Hostname()
+	return domain, nil
+}
+
+func makeRequest(url string) *Response {
+	origin, err := extractDomain(url)
+	if err != nil {
+		fmt.Printf("Erro ao processar a url %s: %v\n", url, err)
+		return &Response{Origin: "", Body: nil}
+	}
+
+	resp, err := resty.New().R().Get(url)
+	if err != nil {
+		fmt.Printf("Erro ao realizar a requisição na url  %s: %v\n", url, err)
+		return &Response{Origin: "", Body: nil}
+	}
+
+	return &Response{Origin: origin, Body: resp}
+}
